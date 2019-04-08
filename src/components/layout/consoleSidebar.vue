b4baf6702923e5e1574c82b5d4cd7d8f2ed83095
Titanxu <titanxuhaha@gmail.com>
2019-04-08 15:29:35 +0800

    init

---
+++ src/components/layout/consoleSidebar.vue
@@ -0,0 +1,108 @@
+<style lang="less">
+.console-sidebar {
+  .el-submenu__title {
+    font-size: 12px;
+    height: 46px;
+    line-height: 46px;
+  }
+  .el-submenu {
+    background: #37424f;
+    border-bottom: 1px solid #414d5c;
+  }
+  .el-submenu__icon-arrow {
+    right: 40px;
+    margin-top: -4px;
+  }
+  .el-icon-message {
+    font-size: 12px;
+  }
+  .el-menu-item {
+    font-size: 12px;
+    height: 42px;
+    line-height: 42px;
+    &.is-active {
+      border-right: none;
+      color: #fff;
+      background: #0099cc !important;
+      &:hover {
+        background: #0099cc !important;
+      }
+    }
+  }
+}
+</style>
+<style scoped lang="less">
+.console-sidebar {
+  position: fixed;
+  top: 50px;
+  bottom: 0px;
+  background-color: #293038;
+  z-index: 102;
+  overflow-x: hidden;
+  overflow-y: auto;
+  height: auto;
+  width: 180px;
+  .sidebar-content {
+    width: 200px;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    background: #293038;
+    .sidebar-fold {
+      height: 30px;
+      width: 140px;
+      background: #394555;
+      color: #aeb9c2;
+      text-align: left;
+      padding: 0 20px;
+      line-height: 30px !important;
+      user-select: none;
+      cursor: pointer;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+    }
+  }
+}
+</style>
+<template>
+  <div class="console-sidebar">
+    <el-menu
+      :unique-opened='true'
+      :router='true'
+      :default-active="defaultActive"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      class="sidebar-content">
+      <div class="sidebar-fold">版本1.0.0</div>
+      <el-menu-item index="/quick">
+        <i class="el-icon-message"></i>
+        快速入门
+      </el-menu-item>
+      <el-submenu index="material">
+        <template slot="title">
+          <i class="el-icon-message"></i>
+          基本信息
+        </template>
+        <el-menu-item index="/dictionary/productMana"><i class="el-icon-document"></i>产品管理</el-menu-item>
+        <el-menu-item index="/dictionary/customerMana"><i class="el-icon-document"></i>客户管理</el-menu-item>
+        <el-menu-item index="/dictionary/brandMana"><i class="el-icon-document"></i>品牌管理</el-menu-item>
+        <el-menu-item index="/dictionary/dictionaryCateMana"><i class="el-icon-document"></i>数据字典管理</el-menu-item>
+      </el-submenu>
+    </el-menu>
+  </div>
+</template>
+<script>
+  export default {
+    components: {
+    },
+    computed: {
+      defaultActive: function() {
+        const that = this;
+        return (
+          (that.$route.meta && that.$route.meta.activePath) || this.$route.path
+        );
+      }
+    }
+  };
+</script>
